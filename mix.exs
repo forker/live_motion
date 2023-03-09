c7a9677e64a45006cc2d19b164b721e88b57d52f
Roman Sergey <roman.sergey+forker@gmail.com>
2023-03-09 20:05:07 +0100

    chore(deps): bump peer deps

--- mix.exs
+++ mix.exs
@@ -31,10 +31,10 @@ defmodule LiveMotion.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
+      {:phoenix, "~> 1.6.0 or ~> 1.7.0"},
       {:phoenix_html, "~> 3.1"},
       {:phoenix_live_view, "~> 0.18"},
-      {:jason, "~> 1.3.0", optional: true},
+      {:jason, "~> 1.3.0 or ~>1.4.0", optional: true},
       {:esbuild, "~> 0.2", only: :dev},
       {:telemetry, "~> 0.4.2 or ~> 1.0"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
